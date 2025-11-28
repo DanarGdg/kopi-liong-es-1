e093730b6f0571d15abf34963abe1c05d6c75d40
Ibra <farhanibra21@gmail.com>
2025-11-28 23:42:54 +0700

    feat: Adding interfaces for Ayat

--- src/types/index.tsx
+++ src/types/index.tsx
@@ -11,6 +11,34 @@ export interface Surah {
     };
 }
 
+export interface SurahNavigation {
+    nomor: number;
+    nama: string;
+    namaLatin: string;
+    jumlahAyat: number;
+}
+
+export interface Ayat {
+    nomorAyat: number;
+    textArab: string;
+    teksLatin: string;
+    textIndonesia: string;
+}
+
+
+export interface AyatListResponse {
+    code: number;
+    nama: string;
+    namaLatin: string;
+    jumlahAyat : number;
+    tempatTurun: string;
+    arti: string;
+    deskripsi: string;
+    ayat: Ayat[];
+    suratSelanjutnya: boolean | SurahNavigation;
+    suratSebelumnya: boolean | SurahNavigation;
+}
+
 export interface SurahListResponse {
     code: number;
     message: string;
